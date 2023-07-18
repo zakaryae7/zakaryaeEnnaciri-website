5954911ba6eaae1e7ffa010a2384f8bce7136243
Zakaryae Ennaciri <118735158+zakaryae7@users.noreply.github.com>
2023-07-18 15:14:10 +0100

    Delete components directory

--- components/loading.js
+++
@@ -1,8 +0,0 @@
-var loading = `<img src="/assets/logos/Untitled-1.png" id="loading-compo">`;
-var loadingCompo = document.getElementById('loading');
-    loadingCompo.innerHTML = loading;
-
-setTimeout(()=>{
-    var loadingCompo = document.getElementById('loading');
-    loadingCompo.style.visibility='hidden';
-},500);
